032d65cacbc8723177b243f1dc18aa05297b4bb2
Amin Shoukat <aminshoukat4@gmail.com>
2020-11-21 00:51:43 +0500

    Counter App with Functional Component

--- src/App.js
+++ src/App.js
@@ -1,69 +1,64 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect  } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Counter from './components/counter';
 import "./App.css";
 
-class App extends Component {
-	state = {
-		initialCounters: [],
-		counters: [
+const App = () => {
+	const [initialCounters, setInitialCounters] = useState([]);
+	const [counters, setCounters] = useState(
+		[
 			{key: 1, value: 0},
 			{key: 2, value: 1},
 			{key: 3, value: 2},
 			{key: 4, value: 3}
 		]
-	};
+	);
 
-	componentDidMount() {
-		const initialCounters = this.state.counters;
-		this.setState({initialCounters});
+	useEffect(() => {
+		const initialCounters = counters;
+		setInitialCounters(initialCounters);
+	}, []);
+	
+
+	const handleIncrement = counter => {
+		const cloneCounters = [...counters];
+		const index = cloneCounters.indexOf(counter);
+		cloneCounters[index] = {...cloneCounters[index]};
+		cloneCounters[index].value++;
+		setCounters(cloneCounters);
 	}
 
-	handleIncrement = counter => {
-		const counters = [...this.state.counters];
-		const index = counters.indexOf(counter);
-		counters[index] = {...counters[index]};
-		counters[index].value++;
-		this.setState({counters});
+	const handleDecrement = counter => {
+		const cloneCounters = [...counters];
+		const index = cloneCounters.indexOf(counter);
+		cloneCounters[index] = {...cloneCounters[index]};
+
+		if(!cloneCounters[index].value < 1)
+			cloneCounters[index].value--;
+
+		setCounters(cloneCounters);
 	}
 
-	handleDecrement = counter => {
-		const counters = [...this.state.counters];
-		const index = counters.indexOf(counter);
-		counters[index] = {...counters[index]};
+	const handleDelete = counter => {
+		const cloneCounters = counters.filter(c => c !== counter);
 
-		if(!counters[index].value < 1)
-			counters[index].value--;
-
-		this.setState({counters});
+		if(!cloneCounters.length < 1)
+			setCounters(cloneCounters);
 	}
 
-
-	handleDelete = counter => {
-		const counters = this.state.counters.filter(c => c !== counter);
-
-		if(!counters.length < 1)
-			this.setState({counters});
+	const handleReset = () => {
+		setCounters(initialCounters);
 	}
 
+	return ( 
+		<div className="App">
+			<button className="btn btn-primary" onClick={() => handleReset()}>Reset</button>
+			{counters.map(counter => 
+				<Counter key={counter.key} counter={counter} handleIncrement={ () => handleIncrement(counter)}  handleDecrement={ () => handleDecrement(counter)}  handleDelete={ () => handleDelete(counter)}  />
+			)}
 
-	handleReset = () => {
-		const counters = this.state.initialCounters;
-		this.setState({counters: counters});
-	}
-
-	render() {
-		return (
-			<div className="App">
-				<button className="btn btn-primary" onClick={() => this.handleReset()}>Reset</button>
-				{this.state.counters.map(counter => 
-					<Counter key={counter.key} counter={counter} handleIncrement={ () => this.handleIncrement(counter)}  handleDecrement={ () => this.handleDecrement(counter)}  handleDelete={ () => this.handleDelete(counter)}  />
-				)}
-
-			</div>
-
-		);
-	}
+		</div>
+	);
 }
 
 export default App;
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,25 +1,23 @@
-import { Component } from 'react';
-
-class Counter extends Component {
-	render() {
-		let {counter, handleIncrement, handleDecrement, handleDelete} = this.props;
-		let badgeClass = 'badge m-2 badge-' + this.setClass(counter.value);
-		return(
-			<div>
-				<span className={badgeClass}>{ (counter.value === 0) ? 'Zero' : counter.value}</span>
-				<button className="btn btn-primary m-2" onClick={handleIncrement}>+</button>
-				<button className="btn btn-secondary m-2" onClick={handleDecrement}>-</button>
-				<button className="btn btn-danger m-2" onClick={handleDelete}>X</button>
-			</div>
-		)
-	}
-
-	setClass = (value) => {
-		if(value == 0)
+const  Counter = ({counter, handleIncrement, handleDecrement, handleDelete}) => {
+	
+	let setClass = (value) => {
+		if(value === 0)
 			return 'warning';
 
 		return 'primary';
 	}
+
+	let badgeClass = 'badge m-2 badge-' + setClass(counter.value);
+
+	return(
+		<div>
+			<span className={badgeClass}>{ (counter.value === 0) ? 'Zero' : counter.value}</span>
+			<button className="btn btn-primary m-2" onClick={handleIncrement}>+</button>
+			<button className="btn btn-secondary m-2" onClick={handleDecrement}>-</button>
+			<button className="btn btn-danger m-2" onClick={handleDelete}>X</button>
+		</div>
+	)
 }
+
 
 export default Counter;
